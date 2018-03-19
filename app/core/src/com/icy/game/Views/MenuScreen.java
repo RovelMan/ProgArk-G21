44438233ce1ca24c4388f462f29e693431b15e6e
RovelMan <jotdeepgahla@hotmail.com>
2018-03-19 13:37:20 +0100

    finished MenuScreen

--- app/core/src/com/icy/game/Views/MenuScreen.java
+++ app/core/src/com/icy/game/Views/MenuScreen.java
@@ -4,6 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.icy.game.IcyGame;
 
 /**
@@ -12,18 +17,83 @@ import com.icy.game.IcyGame;
 
 public class MenuScreen extends Screen {
 
-    public Texture img;
+    private boolean join, create, settings, help;
+    private Stage stage;
 
     public MenuScreen(IcyGame game) {
         super(game);
-        img = new Texture("badlogic.jpg");
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        Image joinBtn = new Image(new Texture("joinBtn.png"));
+        joinBtn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                join = true;
+                return true;
+            }
+        });
+
+        Image createBtn = new Image(new Texture("createBtn.png"));
+        createBtn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                create = true;
+                return true;
+            }
+        });
+        Image settingsBtn = new Image(new Texture("settingsBtn.png"));
+        settingsBtn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                settings = true;
+                return true;
+            }
+        });
+
+        Image helpBtn = new Image(new Texture("helpBtn.png"));
+        helpBtn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                help = true;
+                return true;
+            }
+        });
+
+        Table table = new Table();
+
+        table.center();
+        table.setFillParent(true);
+        table.add(joinBtn).expandX().padBottom(10);
+        table.row();
+        table.add(createBtn).expandX().padBottom(10);
+        table.row();
+        table.add(settingsBtn).expandX().padBottom(10);
+        table.row();
+        table.add(helpBtn).expandX();
+        table.pack();
+        stage.addActor(table);
     }
 
     @Override
     public void handleInput() {
-        if (Gdx.input.justTouched()) {
+        if (join) {
             game.connection.getSocket().emit("test", "heihei");
-            game.setScreen(new PlayScreen(game));
+            //game.setScreen(new PlayScreen(game));
+            System.out.println("Join button pressed");
+            game.setScreen(new JoinScreen(game));
+            dispose();
+        } else if (create) {
+            System.out.println("Create button pressed");
+            game.setScreen(new CreateScreen(game));
+            dispose();
+        } else if (settings) {
+            System.out.println("Settings button pressed");
+            game.setScreen(new SettingsScreen(game));
+            dispose();
+        } else if (help) {
+            System.out.println("Help button pressed");
+            game.setScreen(new TutorialScreen(game));
             dispose();
         }
     }
@@ -40,15 +110,14 @@ public class MenuScreen extends Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-        game.batch.draw(img, 100, 100);
         game.batch.end();
 
-
         update(delta);
+        stage.draw();
     }
 
     @Override
--- app/core/src/com/icy/game/Views/PlayScreen.java
+++ app/core/src/com/icy/game/Views/PlayScreen.java
@@ -36,7 +36,6 @@ public class PlayScreen extends Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         update(delta);
     }
 
