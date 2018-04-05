8197a9d5b96ff3668e3b5cd8b017230f873f56d3
RovelMan <jotdeepgahla@hotmail.com>
2018-04-05 15:31:43 +0200

    added new menu

---
+++ app/android/assets/Map V2/snowCliffRight.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snowCliffRight" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../GameItems/snowCliffRight.png" width="43" height="43"/>
+</tileset>
