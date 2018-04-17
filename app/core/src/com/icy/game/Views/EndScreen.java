204ce0556731aa98c1ee2e186f5153f102d8be3e
RovelMan <jotdeepgahla@hotmail.com>
2018-04-17 18:34:27 +0200

    added end screen

--- app/core/src/com/icy/game/Views/EndScreen.java
+++ app/core/src/com/icy/game/Views/EndScreen.java
@@ -14,6 +14,8 @@ import com.icy.game.Controller.Connection;
 import com.icy.game.IcyGame;
 import com.icy.game.Models.Player;
 
+import org.json.JSONException;
+
 /**
  * Created by jotde on 13.03.2018.
  */
@@ -24,9 +26,14 @@ public class EndScreen implements Screen {
     private Connection connection;
     private static IcyGame game;
     private Texture background;
+    private Player player1;
+    private Player player2;
+    private String room;
 
-    public EndScreen(IcyGame g, Player player1, Player player2) {
+    public EndScreen(IcyGame g, Player player1, Player player2, int winner, String room) {
         game = g;
+        this.player1 = player1;
+        this.player2 = player2;
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         this.connection = game.connection;
@@ -37,13 +44,19 @@ public class EndScreen implements Screen {
         font.getData().setScale(4);
 
         Label gameOverTxt = new Label("Game over", new Label.LabelStyle(font, Color.WHITE));
-        Label playerLostTxt = new Label("Player " + player1.getUsername() + " lost", new Label.LabelStyle(font, Color.WHITE));
+        String winnerName;
+        if (winner == 1) {
+            winnerName = player1.getUsername();
+        } else {
+            winnerName = player2.getUsername();
+        }
+        Label playerWonTxt = new Label("Player " + winnerName + " won!", new Label.LabelStyle(font, Color.WHITE));
 
-        Image backBtn = new Image(new Texture("NavButtons/BACK.png"));
-        Image createBtn = new Image(new Texture("NavButtons/CREATE.png"));
+        Image rematchBtn = new Image(new Texture("NavButtons/REMATCH.png"));
+        Image quitBtn = new Image(new Texture("NavButtons/QUIT.png"));
 
         //Buttons are easily added to this array
-        Image[] buttons = {backBtn, createBtn};
+        Image[] buttons = {rematchBtn, quitBtn};
 
         for (int i = 0; i < buttons.length; i++) {
             final int j = i;
@@ -51,11 +64,19 @@ public class EndScreen implements Screen {
                 @Override
                 public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                     if (j == 0) {
-                        System.out.println("Back button pressed");
-                        game.setScreen(new MenuScreen(game));
+                        System.out.println("Rematch button pressed");
+                        //RESET GAME AND REMEMBER VALUES
+                        game.setScreen(new LobbyScreen(game, player1.getPlayerId(), player1.getUsername(), player2.getUsername(), room));
                         dispose();
                     } else if (j == 1) {
-
+                        System.out.println("Quit button pressed");
+                        //END GAME AND RESET VALUES
+                        try {
+                            connection.leaveLobby(player1.getUsername(), room);
+                        } catch (JSONException e) {
+                            System.out.println("You can't leave!");
+                        }
+                        game.setScreen(new MenuScreen(game));
                     }
                     return false;
                 }
