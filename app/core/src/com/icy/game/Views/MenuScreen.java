145dde09b3ac8b930328694c26ee82528bdaf092
ubuu <henrik-olsvik@outlook.com>
2018-04-21 20:25:51 +0200

    Audio settings and tutorial tweeks
    Audio controls are on menu page. Tutorial has been updated with new characters.

--- app/core/src/com/icy/game/Views/MenuScreen.java
+++ app/core/src/com/icy/game/Views/MenuScreen.java
@@ -2,14 +2,20 @@ package com.icy.game.Views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.icy.game.Controller.SoundController;
 import com.icy.game.IcyGame;
+
+import java.awt.Font;
 
 import static java.lang.Math.round;
 
@@ -18,21 +24,21 @@ public class MenuScreen implements Screen {
     private Stage stage;
     private Texture background;
     private int move;
-
     public MenuScreen() {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         background = new Texture("Backgrounds/menu_background.png");
         move = 0;
+        BitmapFont font = new BitmapFont();
+        font.getData().setScale(4);
 
         Image logo = new Image(new Texture("Logos/2ICYBOIIS_logo_pixelated.png"));
         Image joinBtn = new Image(new Texture("Buttons/JOIN.png"));
         Image createBtn = new Image(new Texture("Buttons/CREATE.png"));
-        Image settingsBtn = new Image(new Texture("Buttons/SETTINGS.png"));
         Image helpBtn = new Image(new Texture("Buttons/HELP.png"));
 
         //Buttons are easily added to this array
-        Image[] buttons = {joinBtn, createBtn, helpBtn, settingsBtn};
+        Image[] buttons = {joinBtn, createBtn, helpBtn};
 
         for (int i = 0; i < buttons.length; i++) {
             final int j = i;
@@ -51,10 +57,6 @@ public class MenuScreen implements Screen {
                         System.out.println("Help button pressed");
                         IcyGame.getInstance().setScreen(new TutorialScreen());
                         dispose();
-                    } else if (j == 3) {
-                        System.out.println("Settings button pressed");
-                        IcyGame.getInstance().setScreen(new SettingsScreen());
-                        dispose();
                     }
                     return false;
                 }
@@ -63,6 +65,29 @@ public class MenuScreen implements Screen {
 
         int width = Gdx.graphics.getWidth();
         int height = Gdx.graphics.getHeight();
+
+        Image audioOn = new Image(new Texture("Buttons/audio_on.png"));
+        audioOn.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Audio on");
+                IcyGame.VOLUME = 1.0f;
+                System.out.println(IcyGame.VOLUME);
+                return true;
+            }
+        });
+        Image audioOff = new Image(new Texture("Buttons/audio_off.png"));
+        audioOff.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Audio off");
+                IcyGame.VOLUME = 0;
+                SoundController.getInstance().stopMusic("music");
+                System.out.println(IcyGame.VOLUME);
+                return true;
+            }
+        });
+
 
         Table table = new Table();
         table.center();
@@ -73,9 +98,16 @@ public class MenuScreen implements Screen {
         table.row();
         table.add(createBtn).expandX().size(width/2, height/8);
         table.row();
-        table.add(helpBtn).expandX().size(width/3, height/8).padBottom(10);
+        table.add(helpBtn).expandX().size(width/3, height/8).padBottom(30);
         table.row();
-        table.add(settingsBtn).expandX().size(width/6, height/8);
+
+        Table subTable = new Table();
+        subTable.add(audioOn).padRight(width/24).padLeft(width/3).padBottom(30).size(IcyGame.WIDTH / 8, IcyGame.HEIGHT / 12);
+        subTable.add(audioOff).padLeft(width/24).padRight(width/3).padBottom(30).size(IcyGame.WIDTH / 8, IcyGame.HEIGHT / 12);
+        subTable.pack();
+
+        table.add(subTable);
+        table.row();
         table.pack();
         stage.addActor(table);
     }
